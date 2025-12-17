458eec8375cadb832c591113c3a0ea804fa13c5d
bala-amypo <bala@amypo.in>
2025-12-17 06:10:51 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,5 +1,7 @@
-
+package com.example.demo.service;
+import com.example.demo.repository.StudentRepository;
 
 public interface StudentService extends StudentRepository{
-
+    public Student addStudent(Student obj);
+    public List<Student> getAllStudents();
 }
--- src/main/java/com/example/demo/service/implement/StudentServiceImplementation.java
+++ src/main/java/com/example/demo/service/implement/StudentServiceImplementation.java
@@ -6,18 +6,21 @@ import java.util.*;
 
 import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.StudentService;
 
 @Service
-public class StudentServiceImplementation {
+public class StudentServiceImplementation implements StudentService {
 
     @Autowired
     StudentRepository repo;
 
+    @Override
     public Student addStudent(Student obj){
         studentDB.put(obj.getId(), obj);
         return repo.save(obj);
     }
 
+    @Override
     public List<Student> getAllStudents(){
         return repo.findAll();
     }
